71b4e3839938325bedf20aabe8e8837b563f09ee
ShinHate <danil.prokopev.83@mail.ru>
2021-04-03 14:25:59 +0700

    Portfolio 4 Commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Cassandra\Schema;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Schema::defaultStringLenght(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
